abe11560c4edad50fb6fa6a6ddff842e02824afb
Gustavo Cotta <gmcotta34@gmail.com>
2020-11-14 13:42:15 -0300

    Installing Angular Material and creating home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HomeComponent } from './home.component';
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+  ],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+  ]
+})
+export class HomeModule { }
